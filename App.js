f54971028a4a32e7c0caf1fb619a78c21a685f59
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2023-01-27 14:21:59 +0100

    modify icons

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { StyleSheet } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import Home from "./screens/Home";
 import Likes from "./screens/Likes";
@@ -13,30 +13,34 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator 
-      screenOptions={({ route }) => ({
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
 
-            if (route.name === 'Home') {
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Likes") {
+              iconName = focused ? "heart" : "heart-outline";
+            } else if (route.name === "MyAccount") {
               iconName = focused
-                ? 'home'
-                : 'home-outline';
-            } else if (route.name === 'Likes') {
-              iconName = focused ? 'heart' : 'heart-outline';
-            } else if (route.name === 'MyAccount') {
-              iconName = focused ? 'account' : 'image-outline';
+                ? "md-information-circle"
+                : "md-information-circle-outline";
             }
             // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color}  />;
+            return <Ionicons name={iconName} size={size} color={color} />;
           },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
         })}
-        >
-        <Tab.Screen name="Camera" component={Home}  options={{ unmountOnBlur: true }} />
-        <Tab.Screen name="Feed" component={Likes} />
-        <Tab.Screen name="Images" component={MyAccount} />
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{ unmountOnBlur: true }}
+        />
+        <Tab.Screen name="Likes" component={Likes} />
+        <Tab.Screen name="MyAccount" component={MyAccount} />
       </Tab.Navigator>
     </NavigationContainer>
   );
@@ -45,8 +49,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
