21d8eca17801f712ddcb3714504451bcd8daeeef
charlinedev <charlinebarbelette@yahoo.fr>
2023-01-27 14:10:11 +0100

    Init

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from "./screens/Home";
+import Likes from "./screens/Likes";
+import MyAccount from "./screens/MyAccount";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator 
+      screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Likes') {
+              iconName = focused ? 'heart' : 'heart-outline';
+            } else if (route.name === 'MyAccount') {
+              iconName = focused ? 'account' : 'image-outline';
+            }
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color}  />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+        >
+        <Tab.Screen name="Camera" component={Home}  options={{ unmountOnBlur: true }} />
+        <Tab.Screen name="Feed" component={Likes} />
+        <Tab.Screen name="Images" component={MyAccount} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Likes.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Text} from 'react-native';
+
+function Likes() {
+    return <Text>Likes</Text>
+};
+export default  Likes;
