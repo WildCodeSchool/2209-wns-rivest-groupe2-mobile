49fec99f6f8a39181910f8c72bb082576122992e
GUILLAUME PINOT <guillaume.pinott@gmail.com>
2023-02-28 12:02:20 +0100

    reset repo

--- screens/Likes.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Text} from 'react-native';
-
-function Likes() {
-    return <Text>Likes</Text>
-};
-export default  Likes;
--- screens/MyAccount.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { Text} from 'react-native';
-
-function MyAccount() {
-    return <Text>MyAccount</Text>
-};
-export default  MyAccount;
