8ecbed97f9c9275379ea37f59a7f30576cb2a665
Jennifer DE LEO <deleo.jennifer1@gmail.com>
2023-02-28 16:44:20 +0100

    add react-native-maps package and implement the map screen

--- App.tsx
+++ App.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import Constants from "expo-constants";
 import Navigator from "./navigation/Navigator";
 import { RecoilRoot } from "recoil";
 
 export default function App() {
+  const { manifest } = Constants;
+
+  const uri =
+    manifest?.debuggerHost &&
+    `http://${manifest.debuggerHost.split(":").shift()}:5000`;
+  const httpLink = createHttpLink({
+    uri: uri,
+  });
+
+  const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <>
+    <ApolloProvider client={client}>
       <RecoilRoot>
         <Navigator />
       </RecoilRoot>
-    </>
+    </ApolloProvider>
   );
 }
--- screens/cityguide/MapScreen.tsx
+++ screens/cityguide/MapScreen.tsx
@@ -1,10 +1,98 @@
-import React from 'react'
-import {Text, StyleSheet} from 'react-native';
+import React, { useCallback, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { gql } from "@apollo/client";
+import { useLazyQuery } from "@apollo/client";
+import { useFocusEffect } from "@react-navigation/native";
+
+export const GET_POI_QUERY = gql`
+  query GetAllPois {
+    getAllPoi {
+      id
+      name
+      address
+      postal
+      type
+      coordinates
+      creationDate
+      pictureUrl
+      websiteURL
+      description
+      priceRange
+      city
+      daysOpen
+      hoursOpen
+      hoursClose
+    }
+  }
+`;
 
 const MapScreen: React.FC = () => {
-  return (
-    <Text>MapScreen</Text>
-  )
-}
+  const [getAllPois, { loading, error }] = useLazyQuery(GET_POI_QUERY);
+  const [pois, setPois] = useState([]);
 
-export default MapScreen 
+  const parisPosition = {
+    latitude: 48.860161,
+    longitude: 2.350041,
+    latitudeDelta: 0.1,
+    longitudeDelta: 0.1,
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      async function fetchPois() {
+        try {
+          const data = await getAllPois();
+
+          const dataPois = [...data.data.getAllPoi];
+
+          setPois(dataPois);
+        } catch (error) {
+          console.log(error);
+          setPois([]);
+        }
+      }
+      fetchPois();
+    }, [])
+  );
+
+  if (loading) {
+    return <Text>Loading</Text>;
+  }
+
+  if (error) {
+    return <Text>{error.message}</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.map} initialRegion={parisPosition}>
+        {pois.map((poi) => (
+          <Marker
+            coordinate={{
+              latitude: poi.coordinates[0],
+              longitude: poi.coordinates[1],
+            }}
+            title={poi.name}
+            description={poi.description}
+            key={poi.id}
+          />
+        ))}
+      </MapView>
+    </View>
+  );
+};
+
+export default MapScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, //the container will fill the whole screen.
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
